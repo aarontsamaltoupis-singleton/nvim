df08520e3caff21f7041a1ad84850d635e22363a
aarontsamaltoupis-singleton <aaron.tsamaltoupis@gmail.com>
2025-11-16 11:12:54 +0100

    new snippets

--- LuaSnip/tex.lua
+++ LuaSnip/tex.lua
@@ -17,11 +17,28 @@ return {
 		t("}"),
 	}),
 
+	--secttions
+	s({ trig = "sec", snippetType = "autosnippet", desc = "custom section", wordTrig = false }, {
+		t("\\section*{"),
+		i(1), -- insert node 1
+		t("}"),
+	}),
+	--environments
+	--
+	--
+	s({ trig = "neq", snippetType = "autosnippet", desc = "neq", wordTrig = false }, { t("\\neq ") }),
 	s({ trig = "dot", snippetType = "autosnippet", desc = "cdot", wordTrig = false }, { t("\\cdot ") }),
 	s({ trig = "fa", snippetType = "autosnippet", desc = "forall", wordTrig = true }, { t("\\forall ") }),
 	s({ trig = "ex", snippetType = "autosnippet", desc = "exists", wordTrig = true }, { t("\\exists ") }),
 	s({ trig = "...", snippetType = "autosnippet", desc = "dots", wordTrig = true }, { t("\\dots ") }),
 	s({ trig = "<=", snippetType = "autosnippet", desc = "leq", wordTrig = true }, { t("\\leq ") }),
+
+	--sets
+	s({ trig = "NN", snippetType = "autosnippet", desc = "naturalnumbers", wordTrig = false }, { t("\\mathbb{N} ") }),
+	s({ trig = "RR", snippetType = "autosnippet", desc = "naturalnumbers", wordTrig = false }, { t("\\mathbb{R} ") }),
+	s({ trig = "QQ", snippetType = "autosnippet", desc = "naturalnumbers", wordTrig = false }, { t("\\mathbb{Q} ") }),
+	s({ trig = "CC", snippetType = "autosnippet", desc = "naturalnumbers", wordTrig = false }, { t("\\mathbb{C} ") }),
+	s({ trig = "ZZ", snippetType = "autosnippet", desc = "naturalnumbers", wordTrig = false }, { t("\\mathbb{Z} ") }),
 
 	--greek letters
 	s({ trig = "@e", snippetType = "autosnippet", desc = "leq", wordTrig = true }, { t("\\varepsilon ") }),
